e98c35f15e146fa4279cb7b38c11347959900856
Alexey Yurasov <13106906+format37@users.noreply.github.com>
2021-06-21 16:19:15 +0300

    Update menger_sponge.py
    save and load interrupted process

--- portal/menger_sponge.py
+++ portal/menger_sponge.py
@@ -3,8 +3,12 @@ import numpy as np
 import math
 import datetime
 import sys
+import time
+import os
 
 depth = int(sys.argv[1])
+start_x = int(sys.argv[2])
+
 
 def mesh_trans(mesh, x, y, z):
     return pymesh.form_mesh(mesh.vertices + [[x, y, z]], mesh.faces)
@@ -13,55 +17,53 @@ def mesh_rotate(mesh, x, y, z, angle):
     rot = pymesh.Quaternion.fromAxisAngle(np.array([x, y, z]), math.pi*2*angle/360)
     return pymesh.form_mesh(np.dot(rot.to_matrix(), mesh.vertices.T).T, mesh.faces)
 
-def menger_sponge(depth):
-    sponge = None
-    z = [-1,2]
+def menger_sponge(depth, start_x):    
+    start = datetime.datetime.now()
+    prev_file = None
+    if start_x == 1:
+        mesh_fractal = pymesh.generate_box_mesh([0,0,0], [1,1,1])    
+    else:
+        mesh_fractal = pymesh.load_mesh("/portal/menger_sponge_"+str(depth)+"_x"+str(start_x)+".stl")
+        start_x += 3
+    z = [-0.1,1.1]
     side = 1
+    operation = 'difference'
+    #operation = 'union'
     for d in range(1, depth+1):
-        log(str(d)+' / '+str(depth)+' sponge holes iteration')
+        log('=== '+str(d)+' / '+str(depth)+' iteration ===')
         side /= 3
-        for x in range(1,3**d,3):
+        for x in range(start_x,3**d,3):            
             for y in range(1,3**d,3):
-                if sponge is None:
-                    sponge = pymesh.generate_box_mesh([side*x,side*y,z[0]], [side*x+side,side*y+side,z[1]])
-                else:
-                    sponge = pymesh.boolean(
-                        sponge, 
-                        pymesh.generate_box_mesh([side*x,side*y,z[0]], [side*x+side,side*y+side,z[1]]), 
-                        operation="union", 
-                        engine="igl"
-                        )
-    return sponge
+                log('[x,y]: ['+str(x)+','+str(y)+'] / '+str(3**d))
+                
+                box_a = pymesh.generate_box_mesh([side*x,side*y,z[0]], [side*x+side,side*y+side,z[1]])
+                
+                box_b = mesh_rotate(mesh = box_a, x = 1, y = 0, z = 0, angle = 90)
+                box_b = mesh_trans(mesh = box_b, x = 0, y = 1, z = 0)
+
+                box_c = mesh_rotate(mesh = box_a, x = 0, y = 1, z = 0, angle = 90)
+                box_c = mesh_trans(mesh = box_c, x = 0, y = 0, z = 1)
+
+                mesh_fractal = pymesh.boolean(mesh_fractal, box_a, operation=operation, engine="igl")
+                mesh_fractal = pymesh.boolean(mesh_fractal, box_b, operation=operation, engine="igl")
+                mesh_fractal = pymesh.boolean(mesh_fractal, box_c, operation=operation, engine="igl")            
+            
+            filename = "/portal/menger_sponge_"+str(d)+"_x"+str(x)+".stl"
+            pymesh.save_mesh(filename, mesh_fractal, ascii=False)
+            if not prev_file is None:
+                print('unlinking', prev_file)
+                os.unlink(prev_file)
+            prev_file = filename
+
+    end = datetime.datetime.now()
+    log('generation time: '+str(end - start))
+    return mesh_fractal
 
 def log(message):
     print(datetime.datetime.now().strftime('%Y.%m.%d %H:%M:%S'), message)
 
-# solid box
-log('0 / '+str(depth)+' iteration')
-mesh_fractal = pymesh.generate_box_mesh([0,0,0], [1,1,1])
-
-# sponge holes generation
-sponge_a = menger_sponge(depth)
-log('rotate b')
-sponge_b = mesh_rotate(mesh = sponge_a, x = 1, y = 0, z = 0, angle = 90)
-log('trans b')
-sponge_b = mesh_trans(mesh = sponge_b, x = 0, y = 1, z = 0)
-log('rotate c')
-sponge_c = mesh_rotate(mesh = sponge_a, x = 0, y = 1, z = 0, angle = 90)
-log('trans c')
-sponge_c = mesh_trans(mesh = sponge_c, x = 0, y = 0, z = 1)
-
-# sponge holes union into single mesh model
-operation = 'union'
-log('union u = a + b')
-sponge_u = pymesh.boolean(sponge_a, sponge_b, operation=operation, engine="igl")
-log('union u = u + c')
-sponge_u = pymesh.boolean(sponge_u, sponge_c, operation=operation, engine="igl")
-
-# boolean difference
-operation = 'difference'
-log(operation+' fractal - u')
-mesh_fractal = pymesh.boolean(mesh_fractal, sponge_u, operation=operation, engine="igl")
+# generation
+mesh_fractal = menger_sponge(depth, start_x)
 
 # save
 log('saving..')
